d2e1d191fa1be7313497b003552c231f80441848
sainyshitha777 <sainyshithathammareddy@gmail.com>
2025-01-27 21:35:28 +0530

    Create routes.py

---
+++ static/routes.py
@@ -0,0 +1,119 @@
+"""
+Product Store Service with UI
+"""
+from flask import jsonify, request, abort
+from flask import url_for  # noqa: F401 pylint: disable=unused-import
+from service.models import Product
+from service.common import status  # HTTP Status Codes
+from . import app
+
+
+######################################################################
+# H E A L T H   C H E C K
+######################################################################
+@app.route("/health")
+def healthcheck():
+    """Let them know our heart is still beating"""
+    return jsonify(status=200, message="OK"), status.HTTP_200_OK
+
+
+######################################################################
+# H O M E   P A G E
+######################################################################
+@app.route("/")
+def index():
+    """Base URL for our service"""
+    return app.send_static_file("index.html")
+
+
+######################################################################
+#  U T I L I T Y   F U N C T I O N S
+######################################################################
+def check_content_type(content_type):
+    """Checks that the media type is correct"""
+    if "Content-Type" not in request.headers:
+        app.logger.error("No Content-Type specified.")
+        abort(
+            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            f"Content-Type must be {content_type}",
+        )
+
+    if request.headers["Content-Type"] == content_type:
+        return
+
+    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        f"Content-Type must be {content_type}",
+    )
+
+
+######################################################################
+# C R E A T E   A   N E W   P R O D U C T
+######################################################################
+@app.route("/products", methods=["POST"])
+def create_products():
+    """
+    Creates a Product
+    This endpoint will create a Product based the data in the body that is posted
+    """
+    app.logger.info("Request to Create a Product...")
+    check_content_type("application/json")
+
+    data = request.get_json()
+    app.logger.info("Processing: %s", data)
+    product = Product()
+    product.deserialize(data)
+    product.create()
+    app.logger.info("Product with new id [%s] saved!", product.id)
+
+    message = product.serialize()
+
+    #
+    # Uncomment this line of code once you implement READ A PRODUCT
+    #
+    # location_url = url_for("get_products", product_id=product.id, _external=True)
+    location_url = "/"  # delete once READ is implemented
+    return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+
+
+######################################################################
+# L I S T   A L L   P R O D U C T S
+######################################################################
+
+#
+# PLACE YOUR CODE TO LIST ALL PRODUCTS HERE
+#
+
+######################################################################
+# R E A D   A   P R O D U C T
+######################################################################
+@app.route("/products/<int:product_id>", methods=["GET"])
+def get_products(product_id):
+    """
+    Retrieve a single Product
+    This endpoint will return a Product based on it's id
+    """
+    app.logger.info("Request to Retrieve a product with id [%s]", product_id)
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+    app.logger.info("Returning product: %s", product.name)
+    return product.serialize(), status.HTTP_200_OK
+
+######################################################################
+# U P D A T E   A   P R O D U C T
+######################################################################
+
+#
+# PLACE YOUR CODE TO UPDATE A PRODUCT HERE
+#
+
+######################################################################
+# D E L E T E   A   P R O D U C T
+######################################################################
+
+
+#
+# PLACE YOUR CODE TO DELETE A PRODUCT HERE
+#
